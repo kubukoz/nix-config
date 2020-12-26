fe2af4293e559895510c7967c8aa27d3a55662a2
Jakub Kozłowski <kubukoz@gmail.com>
2020-12-26 05:30:08 +0100

    Add links

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -68,6 +68,9 @@ in {
   programs.zsh.enable = true; # default shell on catalina
   # programs.fish.enable = true;
 
+  # https://github.com/LnL7/nix-darwin/issues/145
+  # https://github.com/LnL7/nix-darwin/issues/105#issuecomment-567742942
+  # fixed according to https://github.com/LnL7/nix-darwin/blob/b3e96fdf6623dffa666f8de8f442cc1d89c93f60/CHANGELOG
   nix.nixPath = pkgs.lib.mkForce [
     "darwin-config=/Users/kubukoz/.nixpkgs/darwin-configuration.nix:/Users/kubukoz/.nix-defexpr/channels"
   ];
