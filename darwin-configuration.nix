7a80ae0a4e12127c6078e134115e86bead652326
Jakub Kozłowski <kubukoz@gmail.com>
2020-12-26 21:01:06 +0100

    More $HOME-dependent nixPath

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -67,10 +67,12 @@
   # https://github.com/LnL7/nix-darwin/issues/145
   # https://github.com/LnL7/nix-darwin/issues/105#issuecomment-567742942
   # fixed according to https://github.com/LnL7/nix-darwin/blob/b3e96fdf6623dffa666f8de8f442cc1d89c93f60/CHANGELOG
-  nix.nixPath = pkgs.lib.mkForce [
-    # this should be made a little more independent from $HOME
-    "darwin-config=/Users/kubukoz/.nixpkgs/darwin-configuration.nix:/Users/kubukoz/.nix-defexpr/channels"
-  ];
+  nix.nixPath = pkgs.lib.mkForce [{
+    darwin-config = builtins.concatStringsSep ":" [
+      "$HOME/.nixpkgs/darwin-configuration.nix"
+      "$HOME/.nix-defexpr/channels"
+    ];
+  }];
 
   networking.hostName = "kubukoz-pro";
 
