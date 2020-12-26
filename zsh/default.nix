d6aac1ea16c6b49adaab73ea1cbcafba9895aa1a
Jakub Kozłowski <kubukoz@gmail.com>
2020-12-26 21:14:02 +0100

    Move the rest of my .zshrc here

--- zsh/default.nix
+++ zsh/default.nix
@@ -7,13 +7,23 @@
 
     shellInit =
       let plugins = [ "git" "zsh-autosuggestions" "docker" "docker-compose" ];
-      in "plugins=(${builtins.concatStringsSep " " plugins})";
+      in ''
+        plugins=(${builtins.concatStringsSep " " plugins})
+      '';
+
     variables = {
       ZSH_THEME = "powerlevel10k/powerlevel10k";
       POWERLEVEL9K_MODE = "awesome-patched";
       HYPHEN_INSENSITIVE = "true";
       ZSH_HIGHLIGHT_MAXLENGTH = "20";
+      ZSH = "$HOME/.oh-my-zsh";
     };
+
+    interactiveShellInit = ''
+      source $ZSH/oh-my-zsh.sh
+      test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh"
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+    '';
   };
 
 }
