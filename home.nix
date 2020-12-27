b2f3f955d810ed1c5c837d3b8814a5c432525b5b
Jakub Kozłowski <kubukoz@gmail.com>
2020-12-27 22:26:10 +0100

    Configure git

--- home.nix
+++ home.nix
@@ -24,6 +24,34 @@
         };
       };
     };
+    git = {
+      enable = true;
+      userName = "Jakub Kozłowski";
+      userEmail = "kubukoz@gmail.com";
+
+      aliases = {
+        dif = "diff --staged";
+        rekt = "reset --hard HEAD";
+      };
+
+      ignores = [
+        ".metals/"
+        ".history/"
+        "**/project/metals.sbt"
+        ".idea/"
+        ".vscode/"
+        ".bloop/"
+        ".scalafmt.conf"
+        ".bsp/"
+      ];
+
+      includes = [{
+        condition = "gitdir:~/dev/";
+        path = "~/dev/.gitconfig";
+      }];
+
+      extraConfig = { pull = { ff = "only"; }; };
+    };
   };
 
   imports = [ ./zsh ./scala ./node ];
@@ -44,7 +72,6 @@
       dhall-lsp-server
       ffmpeg
       ghc
-      git
       gnuplot
       httpie
       imgcat
