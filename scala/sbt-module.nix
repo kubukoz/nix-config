caf4bc17015e17d7107bdd60699738d6b6fe06ca
Jakub Kozłowski <kubukoz@gmail.com>
2020-12-28 01:04:38 +0100

    Less boilerplate-y sbt module

--- scala/sbt-module.nix
+++ scala/sbt-module.nix
@@ -3,54 +3,49 @@
 with lib;
 
 let
+  mapAttrsWithValues = f2: attrs:
+    map (k: f2 k (attrs."${k}")) (attrNames attrs);
+
+  renderPlugin = plugin: ''
+    addSbtPlugin("${plugin.org}" % "${plugin.artifact}" % "${plugin.version}")
+  '';
+
+  renderCredential = host: cred: ''
+    credentials += Credentials("${cred.realm}", "${host}", "${cred.user}", "${cred.password}")
+  '';
+
   cfg = config.programs.sbt;
-  mapAttrsZipped = f2: attrs:
-    let keys = attrNames attrs;
-    in map (k: f2 k (attrs."${k}")) keys;
-
-  makePlugin = plugin:
-    ''
-      addSbtPlugin("${plugin.org}" % "${plugin.artifact}" % "${plugin.version}")'';
-
-  makeCredential = host: cred:
-    ''
-      credentials += Credentials("${cred.realm}", "${host}", "${cred.user}", "${cred.password}")'';
-
-  plugin = types.submodule {
-    options = {
-      org = mkOption { type = types.str; };
-      artifact = mkOption { type = types.str; };
-      version = mkOption { type = types.str; };
-    };
-  };
-  credential = types.submodule {
-    options = {
-      realm = mkOption { type = types.str; };
-      user = mkOption { type = types.str; };
-      password = mkOption { type = types.str; };
-    };
-  };
   baseSbtConfigPath = ".sbt/1.0";
 in {
   options.programs.sbt = {
     enable = mkEnableOption "sbt";
 
     plugins = mkOption {
-      type = types.listOf plugin;
+      type = types.listOf (types.submodule {
+        options = {
+          org = mkOption { type = types.str; };
+          artifact = mkOption { type = types.str; };
+          version = mkOption { type = types.str; };
+        };
+      });
       default = [ ];
+      apply = v: concatStrings (map renderPlugin v);
     };
     credentials = mkOption {
-      type = types.attrsOf credential;
+      type = types.attrsOf (types.submodule {
+        options = {
+          realm = mkOption { type = types.str; };
+          user = mkOption { type = types.str; };
+          password = mkOption { type = types.str; };
+        };
+      });
       default = [ ];
+      apply = v: concatStrings (mapAttrsWithValues renderCredential v);
     };
   };
 
   config = mkIf cfg.enable {
-
-    home.file."${baseSbtConfigPath}/plugins/plugins.sbt".text =
-      concatStringsSep "\n" (map makePlugin cfg.plugins);
-
-    home.file."${baseSbtConfigPath}/credentials.sbt".text =
-      concatStringsSep "\n" (mapAttrsZipped makeCredential cfg.credentials);
+    home.file."${baseSbtConfigPath}/plugins/plugins.sbt".text = cfg.plugins;
+    home.file."${baseSbtConfigPath}/credentials.sbt".text = cfg.credentials;
   };
 }
