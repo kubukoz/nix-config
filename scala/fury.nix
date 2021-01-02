bb08f67719ed72418c687d677e7d8408b052c9a2
Jakub Kozłowski <kubukoz@gmail.com>
2021-01-02 05:20:50 +0100

    wip fury

--- scala/fury.nix
+++ scala/fury.nix
@@ -5,7 +5,7 @@ let
   version = "0.18.29";
   ipfsHash = "QmP8PFjpKsbAP3P9EGhkYqLEGrhxg7WfXu536cTjAFNm1b";
   ipfsGateway = "https://gateway.pinata.cloud/ipfs";
-  remoteSrc = builtins.fetchTarball {
+  remoteSrc = builtins.fetchurl {
     url = "${ipfsGateway}/${ipfsHash}";
     sha256 = "1iwsvwvlijx2i9ym1v3w14v77a99kv1ykj84h3hf0qlpl073pp73";
   };
@@ -13,10 +13,12 @@ in stdenv.mkDerivation {
   name = "fury";
   buildInputs = [ gnutar coursier bloop ipfs ];
 
-  # src = remoteSrc
-  src = "${./.}/fury-${version}.tar.gz";
+  src = remoteSrc;
+  # src = "${./.}/fury-${version}.tar.gz";
 
   unpackPhase = ''
+    echo $src
+    ls $src
     tar -xzf $src
   '';
 
