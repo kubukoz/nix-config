38b9a7a5f4f8081f1e523e654a2821febb052c62
Jakub Kozłowski <kubukoz@gmail.com>
2021-01-29 21:11:55 +0100

    Use unstable from channel

--- unstable.nix
+++ unstable.nix
@@ -1,9 +1,1 @@
-args:
-
-let
-  rev = "e19491e24c98c9da4900b7c890ded47f7bfb58ee";
-  src = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.zip";
-    sha256 = "1wfdndmic20n8srszh79pmic5268amj01xh1rnm09yrq9z2k7wiy";
-  };
-in import src ({ config.allowUnfree = true; } // args)
+args: import <unstable> ({ config.allowUnfree = true; } // args)
