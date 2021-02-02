f696500dccd2adf8a2ea5868b6ba698e506ddbab
Jakub Kozłowski <kubukoz@gmail.com>
2021-02-02 21:55:24 +0100

    Add nix-dss and bamc package from that

--- programs/aws/default.nix
+++ programs/aws/default.nix
@@ -1,7 +1,16 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+
+let
+  nix-dss = import (builtins.fetchGit {
+    url = "git@github.bamtech.co:jkoslowski/nix-dss";
+    ref = "v0.0.5";
+    rev = "6557306249078fc8f0f41f30821bb45ef3e0dc4c";
+  }) { inherit pkgs; };
+in {
   home.packages = [
     pkgs.awscli
     pkgs.ssm-session-manager-plugin
     (pkgs.callPackage ./ssm-helpers.nix { })
+    nix-dss.bamc
   ];
 }
