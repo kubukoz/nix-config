e122409b38abcc5e202f41fda8cd4a59f0468be4
Jakub Kozłowski <kubukoz@gmail.com>
2021-02-18 00:14:15 +0100

    Bump signal

--- derivations/signal.nix
+++ derivations/signal.nix
@@ -1,13 +1,13 @@
 { callPackage }:
 let
-  version = "1.39.6";
+  version = "1.40.0";
 in
 callPackage ./mac-dmg-app.nix {
   name = "signal-desktop-mac-${version}";
   src = builtins.fetchurl {
     url =
       "https://updates.signal.org/desktop/signal-desktop-mac-${version}.dmg";
-    sha256 = "0sayvjimxavd4c4h7n4i78rny4nk94cnlblwq90kw0ypfkx8garf";
+    sha256 = "170v63bmgyis4h5bs9dynzkvps620hngxbz70jsm75w8abvvksln";
   };
   passthru = {
     inherit version;
