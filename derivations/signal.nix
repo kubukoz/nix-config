829b42389b840e87c767431160400911377875df
Jakub Kozłowski <kubukoz@gmail.com>
2021-02-23 01:06:37 +0100

    Update signal

--- derivations/signal.nix
+++ derivations/signal.nix
@@ -1,13 +1,13 @@
 { callPackage }:
 let
-  version = "1.40.0";
+  version = "1.40.1";
 in
 callPackage ./mac-dmg-app.nix {
   name = "signal-desktop-mac-${version}";
   src = builtins.fetchurl {
     url =
       "https://updates.signal.org/desktop/signal-desktop-mac-${version}.dmg";
-    sha256 = "170v63bmgyis4h5bs9dynzkvps620hngxbz70jsm75w8abvvksln";
+    sha256 = "0xk5gsxlc6imlp5lpkwv1416ryn297sar9hm4xrp4ikvkwn15f1z";
   };
   passthru = {
     inherit version;
