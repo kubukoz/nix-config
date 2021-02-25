06f3f464c8891ab1f7331521d5616cd9bcaed231
Jakub Kozłowski <kubukoz@gmail.com>
2021-02-25 14:30:10 +0100

    Move metals config to new file

--- vscode/default.nix
+++ vscode/default.nix
@@ -57,38 +57,6 @@ let
     formatterFor = [ "typescript" "typescriptreact" "json" "jsonc" ];
   };
 
-  metals = configuredExtension {
-    extension = vscode-extensions.scalameta.metals;
-    formatterFor = [ "scala" ];
-    settings = {
-      "metals.serverVersion" = "0.9.10+151-aaa761e5-SNAPSHOT";
-      "metals.serverProperties" = [ "-Dmetals.verbose" ];
-      "files.watcherExclude" = { "**/.metals" = true; };
-    };
-    keybindings = [
-      {
-        key = "cmd+i";
-        command = "metals.build-import";
-      }
-      {
-        key = "shift+alt+cmd+i";
-        command = "metals.toggle-implicit-conversions-and-classes";
-      }
-      {
-        key = "shift+cmd+i";
-        command = "metals.toggle-implicit-parameters";
-      }
-      {
-        key = "shift+alt+cmd+t";
-        command = "metals.toggle-show-inferred-type";
-      }
-      {
-        key = "ctrl+shift+cmd+up";
-        command = "metals.goto-super-method";
-      }
-    ];
-  };
-
   oneDarkPro = configuredExtension {
     extension = vscode-extensions.zhuangtongfa.material-theme;
     settings =
@@ -198,7 +166,7 @@ in
   imports = [
     baseSettings
     scala
-    metals
+    ./metals.nix
     oneDarkPro
     prettier
     markdown
---
+++ vscode/metals.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+let
+  vscode-lib = import ./lib.nix;
+  inherit (vscode-lib) configuredExtension;
+  inherit (pkgs) vscode-utils vscode-extensions;
+in
+configuredExtension {
+  extension = vscode-extensions.scalameta.metals;
+  formatterFor = [ "scala" ];
+  settings = {
+    "metals.serverVersion" = "0.9.10+151-aaa761e5-SNAPSHOT";
+    "metals.serverProperties" = [ "-Dmetals.verbose" ];
+    "files.watcherExclude" = { "**/.metals" = true; };
+  };
+  keybindings = [
+    {
+      key = "cmd+i";
+      command = "metals.build-import";
+    }
+    {
+      key = "shift+alt+cmd+i";
+      command = "metals.toggle-implicit-conversions-and-classes";
+    }
+    {
+      key = "shift+cmd+i";
+      command = "metals.toggle-implicit-parameters";
+    }
+    {
+      key = "shift+alt+cmd+t";
+      command = "metals.toggle-show-inferred-type";
+    }
+    {
+      key = "ctrl+shift+cmd+up";
+      command = "metals.goto-super-method";
+    }
+  ];
+}
