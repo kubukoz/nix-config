5d6bb17c7733a4b2037d421ceac8b3e9a80cdf5f
Jakub Kozłowski <kubukoz@gmail.com>
2021-05-07 17:39:38 +0200

    Remove vscode package

--- vscode/default.nix
+++ vscode/default.nix
@@ -8,6 +8,7 @@ let
 
   baseSettings = mkVscodeModule {
     enable = true;
+    package = pkgs.hello // { pname = pkgs.vscode.pname; };
     userSettings = import ./global-settings.nix;
     keybindings = import ./global-keybindings.nix { inherit vscode-lib; };
 
