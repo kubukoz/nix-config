193dd489262ecdea3035a5094017f9f1b7338295
Jakub Kozłowski <kubukoz@gmail.com>
2021-06-13 04:14:37 +0200

    Add hmm completions

--- derivations/hmm.nix
+++ derivations/hmm.nix
@@ -1,14 +1,18 @@
-{ rustPlatform }:
+{ rustPlatform, installShellFiles }:
 
 rustPlatform.buildRustPackage {
   name = "hmm";
-  buildInputs = [];
+  buildInputs = [ installShellFiles ];
   cargoSha256 = "14x31hl14iw7mw0jp652kam4jgbh4036ph88mqgdqyhh4vf07n2z";
 
   src = builtins.fetchGit {
     url = "git@github.com:kubukoz/hmm";
-    ref = "v0.2.0";
-    rev = "9312d360309c3eb0be0a9059f5372b8b53ff54e7";
+    ref = "v0.2.1";
+    rev = "3e24cf64d91d0398e3eecf29249c5b551fbb7b33";
   };
+
+  postInstall = ''
+    installShellCompletion --name _hmm completions/zsh/_hmm
+  '';
 
 }
