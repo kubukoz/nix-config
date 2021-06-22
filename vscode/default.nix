76a66e72e20f01edf27c5f668d745842ecd4f05d
Jakub Kozłowski <kubukoz@gmail.com>
2021-06-22 04:21:57 +0200

    color smithy functions

--- vscode/default.nix
+++ vscode/default.nix
@@ -93,6 +93,7 @@ let
           # These have been stolen from https://github.com/joshdick/onedark.vim
           yellow = "#e5c07b";
           blue = "#61afef";
+          light-grey = "#abb2bf";
         in
           {
             "workbench.colorTheme" = themeName;
@@ -102,11 +103,23 @@ let
                 functions = yellow;
                 variables = yellow;
                 types = blue;
+                "textMateRules" = [
+                  {
+                    "scope" = [ "support.function" "source.smithy" ];
+                    "settings" = {
+                      "foreground" = light-grey;
+                    };
+                  }
+                ];
               };
+
 
             "editor.semanticTokenColorCustomizations"."[${themeName}]"."rules" = {
               "variable:rust" = {
                 "foreground" = yellow;
+              };
+              "function:smithy" = {
+                "foreground" = blue;
               };
             };
           };
