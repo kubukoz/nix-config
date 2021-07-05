56438e230d1381483a1ab4ea69b5c4f35b4c88b1
Jakub Kozłowski <kubukoz@gmail.com>
2021-07-06 01:04:22 +0200

    same

--- vscode/extensions/auto.nix
+++ vscode/extensions/auto.nix
@@ -1,6 +1,7 @@
 #
 # This file was written by hmm (Home Manager Manager).
 # Don't edit it manually!
+# This file should always parse as an array of string literals.
 #
 [
   "baccata.scaladex-search"
