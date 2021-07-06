f3c59b5a6a8d84af342d8a7988741921b631e3c9
Jakub Kozłowski <kubukoz@gmail.com>
2021-07-06 02:24:56 +0200

    Sort vscode managed by publisher then name

--- vscode/extensions/managed.nix
+++ vscode/extensions/managed.nix
@@ -1,27 +1,9 @@
 [
   {
-    name = "vscode-remote-extensionpack";
-    publisher = "ms-vscode-remote";
-    version = "0.20.0";
-    sha256 = "04wrbfsb8p258pnmqifhc9immsbv9xb6j3fxw9hzvw6iqx2v3dbi";
-  }
-  {
     name = "nickel-syntax";
     publisher = "kubukoz";
     version = "0.0.1";
     sha256 = "010zn58j9kdb2jpxmlfyyyais51pwn7v2c5cfi4051ayd02b9n3s";
-  }
-  {
-    name = "graph-buddy";
-    publisher = "virtuslab";
-    version = "0.4.21";
-    sha256 = "0a7rl21r0pw6fh7ykh4y7cxxvbxah281i3nygf4g0zhzl3a7y263";
-  }
-  {
-    name = "indent-rainbow";
-    publisher = "oderwat";
-    version = "7.5.0";
-    sha256 = "0zm1m46gm4hl56y9555h3rg8xznygmxb5qlq9yl5wxdjsjcia4qk";
   }
   {
     name = "rust-analyzer";
@@ -34,5 +16,23 @@
     publisher = "ms-vsliveshare";
     version = "1.0.3577";
     sha256 = "1aprpx2mhkdg26x35hxlnlr0hx28znha1y6wrrd4cw549scssp9a";
+  }
+  {
+    name = "vscode-remote-extensionpack";
+    publisher = "ms-vscode-remote";
+    version = "0.20.0";
+    sha256 = "04wrbfsb8p258pnmqifhc9immsbv9xb6j3fxw9hzvw6iqx2v3dbi";
+  }
+  {
+    name = "indent-rainbow";
+    publisher = "oderwat";
+    version = "7.5.0";
+    sha256 = "0zm1m46gm4hl56y9555h3rg8xznygmxb5qlq9yl5wxdjsjcia4qk";
+  }
+  {
+    name = "graph-buddy";
+    publisher = "virtuslab";
+    version = "0.4.21";
+    sha256 = "0a7rl21r0pw6fh7ykh4y7cxxvbxah281i3nygf4g0zhzl3a7y263";
   }
 ]
