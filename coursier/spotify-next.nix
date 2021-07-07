e4f1fd35d668147d2c6c5462fddf5a8e70c98f75
Jakub Kozłowski <kubukoz@gmail.com>
2021-07-07 23:43:15 +0200

    Update spotify-next

--- coursier/spotify-next.nix
+++ coursier/spotify-next.nix
@@ -1,10 +1,10 @@
 { coursier-tools }:
 
 let
-  version = "1.3.5";
+  version = "1.4.0";
 in
 coursier-tools.coursierBootstrap {
   name = "spotify-next";
   inherit version;
-  artifact = "com.kubukoz:spotify-next_2.13:${version}";
+  artifact = "com.kubukoz:spotify-next_3:${version}";
 }
