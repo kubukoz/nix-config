b02cadaaddf68e56e180b7168fe835413c54a00c
Jakub Kozłowski <kubukoz@gmail.com>
2021-07-11 00:20:41 +0200

    oops

--- derivations/hmm.nix
+++ derivations/hmm.nix
@@ -3,12 +3,12 @@
 rustPlatform.buildRustPackage {
   name = "hmm";
   buildInputs = [ installShellFiles ];
-  cargoSha256 = "077m3v3g4g2cb4vp3yrjaizfiixsny2cwkix6hdy6ih3qpm6wl2r";
+  cargoSha256 = "11n63qxncdlvy836z9p6nd2rwnbf2fh46rfj0inrl3385yfm2ni7";
 
   src = builtins.fetchGit {
     url = "git@github.com:kubukoz/hmm";
     ref = "v0.4.0";
-    rev = "11n63qxncdlvy836z9p6nd2rwnbf2fh46rfj0inrl3385yfm2ni7";
+    rev = "9a9ed8f8b9227bb8c2751c19338d838d8ff5a82f";
   };
 
   postInstall = ''
