5e6cdd230444f90b28cfa83debc84dd36bcd0c31
Jakub Kozłowski <kubukoz@gmail.com>
2021-07-11 00:29:15 +0200

    Fix hmm

--- derivations/hmm.nix
+++ derivations/hmm.nix
@@ -1,8 +1,11 @@
-{ rustPlatform, installShellFiles }:
+{ rustPlatform, installShellFiles, stdenv, darwin }:
 
 rustPlatform.buildRustPackage {
   name = "hmm";
-  buildInputs = [ installShellFiles ];
+  buildInputs = let
+    libs = if (stdenv.isDarwin) then [ darwin.apple_sdk.frameworks.Security ] else [];
+  in
+    [ installShellFiles ] ++ libs;
   cargoSha256 = "11n63qxncdlvy836z9p6nd2rwnbf2fh46rfj0inrl3385yfm2ni7";
 
   src = builtins.fetchGit {
