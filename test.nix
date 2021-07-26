d4e28b14bf3c54a098828450fa5160f15f82faff
Jakub Kozłowski <kubukoz@gmail.com>
2021-07-26 19:56:42 +0200

    Remove unused file

--- test.nix
+++
@@ -1,9 +0,0 @@
-let
-  programsFromTextFile = { pkgs, file }:
-    let
-      autoProgramPaths = map (pkgs.lib.splitString ".") (pkgs.lib.splitString "\n" (pkgs.lib.fileContents file));
-    in
-      (map (path: pkgs.lib.attrsets.getAttrFromPath recurse path pkgs) autoProgramPaths);
-
-in
-programsFromTextFile { inherit pkgs; file = ./programs/auto.txt; }
