0c9a991654b7fc2b11ea7fff38808379bc490ba4
Jakub Kozłowski <kubukoz@gmail.com>
2021-09-22 01:36:46 +0200

    Bump nixpkgs unstable

--- unstable.nix
+++ unstable.nix
@@ -1,6 +1,6 @@
 import (
   builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/2c2b33c326a3faf5dc86e373dfafe0dcf3eb5136.tar.gz";
-    sha256 = "0wpfllarf1s0bbs85f9pa4nihf94hscpzvk5af4a1zxgq2l9c8r6";
+    url = "https://github.com/NixOS/nixpkgs/archive/3175ed5b174f6d802aafa9ed363c4adfa204de32.tar.gz";
+    sha256 = "03hygiy54pz36b4k7m3z55ync2zfiah82ljx7qcgjvij98db5v8r";
   }
 )
