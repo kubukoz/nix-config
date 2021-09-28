21bc3cb9bd5f60490d0d5586c0f2b421a32b1a1a
Jakub Kozłowski <kubukoz@gmail.com>
2021-09-28 22:11:00 +0200

    Add zsh-nix-shell

--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -33,6 +33,16 @@ in
         src = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
         file = "${name}.zsh"; # override default with .plugin.zsh
       }
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.4.0";
+          sha256 = "037wz9fqmx0ngcwl9az55fgkipb745rymznxnssr3rx9irb6apzg";
+        };
+      }
     ];
 
     localVariables = {
