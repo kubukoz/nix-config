00298e876d52cb7512010def15872530e68860d0
Jakub Kozłowski <kubukoz@gmail.com>
2021-09-30 20:53:14 +0200

    Add hmm to niv, update nix-dss

--- derivations/hmm.nix
+++ derivations/hmm.nix
@@ -1,5 +1,8 @@
 { rustPlatform, installShellFiles, openssl, stdenv, darwin }:
 
+let
+  sources = import ../nix/sources.nix;
+in
 rustPlatform.buildRustPackage {
   name = "hmm";
   buildInputs = let
@@ -8,11 +11,7 @@ rustPlatform.buildRustPackage {
     [ installShellFiles openssl ] ++ libs;
   cargoSha256 = "19khl2ssdrih10m6m2ip795h299zwkms513xdais10b6cdwrmn66";
 
-  src = builtins.fetchGit {
-    url = "git@github.com:kubukoz/hmm";
-    ref = "v0.5.1";
-    rev = "72e37f5196204104043c05bd98f13b32a99ac64c";
-  };
+  src = sources.hmm;
 
   postInstall = ''
     installShellCompletion --name _hmm completions/zsh/_hmm
