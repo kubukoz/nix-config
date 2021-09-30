41a5deef5bd07930314f0ae9518881433abf71b5
Jakub Kozłowski <kubukoz@gmail.com>
2021-09-30 18:46:26 +0200

    Update unstable

--- unstable.nix
+++
@@ -1,6 +0,0 @@
-import (
-  builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/3175ed5b174f6d802aafa9ed363c4adfa204de32.tar.gz";
-    sha256 = "03hygiy54pz36b4k7m3z55ync2zfiah82ljx7qcgjvij98db5v8r";
-  }
-)
