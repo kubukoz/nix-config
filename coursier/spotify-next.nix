0f9eb22f52f90cb2f856b274f54fe3b2cc7c87ca
Jakub Kozłowski <kubukoz@gmail.com>
2021-10-22 03:41:50 +0200

    Update spotify-next

--- coursier/spotify-next.nix
+++ coursier/spotify-next.nix
@@ -1,11 +1,10 @@
 { coursier-tools, installShellFiles, lib }:
 
 let
-  version = "1.7.1";
+  version = "1.7.2";
   pname = "spotify-next";
   completions = builtins.fetchurl {
-    url = lib.warn "TODO: use actual version in URL"
-      "https://raw.githubusercontent.com/kubukoz/spotify-next/v1.7.2/completions.zsh";
+    url = "https://raw.githubusercontent.com/kubukoz/spotify-next/v${version}/completions.zsh";
     sha256 = "0ip60xbq8byik7hjq7lv4w5fk8aawwdw43gxjmwy51n52qdsa23a";
   };
 in
