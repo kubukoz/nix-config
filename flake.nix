b93d3544ecdf2d438227d9ea072b1acd799c5ba7
Jakub Kozłowski <kubukoz@gmail.com>
2021-11-18 03:34:58 +0100

    Use nix-dss from flake

--- flake.nix
+++ flake.nix
@@ -8,16 +8,18 @@
     home-manager.url = "github:nix-community/home-manager?rev=5559ef002306dde0093f3d329725259cada9ed41";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     unstable.url = "github:nixos/nixpkgs?rev=b42d15105172721b2ec34981d574cab7956e80a9";
+    nix-dss.url = "git+ssh://git@github.bamtech.co/jkozlowski/nix-dss?ref=flakes";
+    nix-dss.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, darwin, nixpkgs, home-manager, unstable }:
+  outputs = { self, darwin, nixpkgs, home-manager, unstable, nix-dss }:
     {
       darwinConfigurations.kubukoz-work = darwin.lib.darwinSystem {
         system = "x86_64-darwin";
         modules = [ ./darwin-configuration.nix ];
         specialArgs = {
           machine = import ./system/machines/work.nix;
-          inherit home-manager unstable;
+          inherit home-manager unstable nix-dss;
         };
       };
     };
