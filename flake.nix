182cebf692dbf1e1dbbd1eaf6504d0e46354546e
Jakub Kozłowski <kubukoz@gmail.com>
2021-11-22 17:40:07 +0100

    Update HM, unstable

--- flake.nix
+++ flake.nix
@@ -5,9 +5,9 @@
     nixpkgs.url = "github:nixos/nixpkgs?rev=2182da1de8081da92f3c5c04e3fa5e5452266afc";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager?rev=5559ef002306dde0093f3d329725259cada9ed41";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    unstable.url = "github:nixos/nixpkgs?rev=b42d15105172721b2ec34981d574cab7956e80a9";
+    unstable.url = "github:nixos/nixpkgs";
     nix-dss.url = "git+ssh://git@github.bamtech.co/jkozlowski/nix-dss?ref=flakes";
     nix-dss.inputs.nixpkgs.follows = "nixpkgs";
     hmm.url = "github:kubukoz/hmm";
