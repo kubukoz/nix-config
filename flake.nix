2bdb621a8a93bbf1aaf1d334caabf9a5e573e81b
Jakub Kozłowski <kubukoz@gmail.com>
2021-12-05 01:45:42 +0100

    Add remote builder

--- flake.nix
+++ flake.nix
@@ -25,21 +25,33 @@
         };
       };
       darwinConfigurations.kubukoz-max =
-        let arm-overrides = final: prev:
-          let
-            mkPackages = source: import source {
-              localSystem = "x86_64-darwin";
-              config.permittedInsecurePackages = [
-                "openssl-1.0.2u"
-              ];
+        let
+          machine = import ./system/machines/max.nix;
+
+          arm-overrides = final: prev:
+            let
+              mkPackages = source: import source {
+                localSystem = "x86_64-darwin";
+                config.permittedInsecurePackages = [
+                  "openssl-1.0.2u"
+                ];
+              };
+
+              pkgs_x86 = mkPackages nixpkgs;
+            in
+
+            {
+              inherit (pkgs_x86) niv openconnect scala-cli nix-tree;
+              bloop = pkgs_x86.bloop.override { jre = prev.openjdk11; };
             };
-
-            pkgs_x86 = mkPackages nixpkgs;
-          in
-
-          {
-            inherit (pkgs_x86) niv openconnect scala-cli nix-tree;
-            bloop = pkgs_x86.bloop.override { jre = prev.openjdk11; };
+          distributed-builds = {
+            nix = {
+              distributedBuilds = true;
+              buildMachines = let builders = import ./semisecret-builders.nix; in
+                [
+                  (builders.jk-nixos { sshKey = "/Users/${machine.username}/.ssh/id_ed25519"; })
+                ];
+            };
           };
         in
         darwin.lib.darwinSystem {
@@ -55,8 +67,7 @@
             ./work/vpn/configuration.nix
           ];
           specialArgs = {
-            machine = import ./system/machines/max.nix;
-            inherit home-manager nix-dss hmm;
+            inherit home-manager hmm machine;
           };
         };
     };
