480c1391e799e45361a061902d551c522f4fff60
Jakub Kozłowski <kubukoz@gmail.com>
2021-12-21 19:41:17 +0100

    Switch to openjdk17
    zulu11.48.21-ca-jdk: 11.0.11 → ∅, [32;1m-285791.1 KiB[0m
    zulu17.30.15-ca-jdk: ∅ → 17.0.1, [31;1m+318107.6 KiB[0m

--- overlays/jvm.nix
+++ overlays/jvm.nix
@@ -1,6 +1,6 @@
 self: super:
 let
-  jre = if super.stdenv.isAarch64 then super.openjdk11 else super.graalvm11-ce;
+  jre = if super.stdenv.isAarch64 then super.openjdk17 else super.graalvm11-ce;
   jdk = jre;
 in
 {
