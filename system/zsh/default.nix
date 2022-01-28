54ea53e344b89758d42adc67713c62109a9fdaec
Jakub Kozłowski <kubukoz@gmail.com>
2022-01-29 00:20:43 +0100

    Remove workaround for zsh completions

--- system/zsh/default.nix
+++ system/zsh/default.nix
@@ -3,7 +3,6 @@
     enable = true;
     enableSyntaxHighlighting = true;
     enableFzfHistory = true;
-    # To avoid conflict with built-in completions in Nix 2.4
-    enableCompletion = false;
+    enableCompletion = true;
   };
 }
