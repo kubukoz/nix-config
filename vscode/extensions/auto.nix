9cce95d5212a2c0a0351b7f029ff771968375b9d
Jakub Kozłowski <kubukoz@gmail.com>
2022-02-03 19:11:30 +0100

    (hmm) Add kubukoz.nickel-syntax, silvenon.mdx, benfradet.vscode-unison, humao.rest-client, Arjun.swagger-viewer, disneystreaming.smithy

--- vscode/extensions/auto.nix
+++ vscode/extensions/auto.nix
@@ -4,19 +4,25 @@
 # This file should always parse as an array of string literals.
 #
 [
+  "Arjun.swagger-viewer"
   "arrterian.nix-env-selector"
   "baccata.scaladex-search"
+  "benfradet.vscode-unison"
   "codezombiech.gitignore"
+  "disneystreaming.smithy"
   "github.copilot"
   "github.vscode-pull-request-github"
   "graphql.vscode-graphql"
   "haskell.haskell"
+  "humao.rest-client"
   "justusadam.language-haskell"
+  "kubukoz.nickel-syntax"
   "matklad.rust-analyzer"
   "mishkinf.goto-next-previous-member"
   "ms-azuretools.vscode-docker"
   "redhat.vscode-yaml"
   "shyykoserhiy.vscode-spotify"
+  "silvenon.mdx"
   "timonwong.shellcheck"
   "usernamehw.errorlens"
 ]
