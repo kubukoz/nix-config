434dd74f5ac926c4b46e10af0dd50b5dc3d7ea12
Jakub Kozłowski <kubukoz@gmail.com>
2022-02-11 01:55:46 +0100

    foo

--- compare.sh
+++ compare.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 
-ACTION="$1"
-
-echo "Running compare.sh, will run $ACTION"
-
 nix build ~/.nixpkgs#darwinConfigurations.${HOSTNAME}.system
 nix store diff-closures /run/current-system ./result
 
+
+ACTION="$1"
 if [ "$ACTION" = "commit" ]; then
-    git commit -m "$(nix store diff-closures /run/current-system ./result | grep →)"
+    # remaining args after $1
+    git commit -m "$(nix store diff-closures /run/current-system ./result | grep →)" "${@:2}"
 fi
