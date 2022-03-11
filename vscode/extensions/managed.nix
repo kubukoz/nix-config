27e25f0969c21de6384ba782261d55c834dca143
Jakub Kozłowski <kubukoz@gmail.com>
2022-03-11 18:27:07 +0100

    (hmm) Add scalameta.metals:  -> 1.13.1

--- vscode/extensions/managed.nix
+++ vscode/extensions/managed.nix
@@ -3,4 +3,9 @@
 # Don't edit it manually!
 # This file should always parse as an array of attrsets.
 #
-[ ]
+[{
+  name = "metals";
+  publisher = "scalameta";
+  version = "1.13.1";
+  sha256 = "1rxv2h5ysa08289gp1pr0vpinablhir44a6s2qmyfpb12bckrl91";
+}]
