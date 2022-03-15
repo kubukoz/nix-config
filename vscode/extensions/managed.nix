0f4daf800dd44054987989b75710aaf83a1db1dd
Jakub Kozłowski <kubukoz@gmail.com>
2022-03-16 00:31:42 +0100

    (hmm) Update scalameta.metals: 1.13.2 -> 1.13.3

--- vscode/extensions/managed.nix
+++ vscode/extensions/managed.nix
@@ -6,6 +6,6 @@
 [{
   name = "metals";
   publisher = "scalameta";
-  version = "1.13.2";
-  sha256 = "1dzi0y485h7vqrzwiq346h22dw8ra8g6y23y1gin9dv4psvy97gj";
+  version = "1.13.3";
+  sha256 = "11iy93rkwy4vzd71zgs731r1a2z338m3p2w0zzavzja7vjwb5868";
 }]
