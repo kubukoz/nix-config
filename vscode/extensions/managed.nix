ef59b0f43e834a737827ce00d8b770ff0e4f31da
Jakub Kozłowski <kubukoz@gmail.com>
2022-03-23 17:49:50 +0100

    (hmm) Add flix.flix: 0.64.0

--- vscode/extensions/managed.nix
+++ vscode/extensions/managed.nix
@@ -3,9 +3,17 @@
 # Don't edit it manually!
 # This file should always parse as an array of attrsets.
 #
-[{
-  name = "metals";
-  publisher = "scalameta";
-  version = "1.13.6";
-  sha256 = "0sdxcvzgw4rc9lg6dpa2jh1lmfr1zhk5jalcvlyj2g56y1visi4l";
-}]
+[
+  {
+    name = "metals";
+    publisher = "scalameta";
+    version = "1.13.6";
+    sha256 = "0sdxcvzgw4rc9lg6dpa2jh1lmfr1zhk5jalcvlyj2g56y1visi4l";
+  }
+  {
+    name = "flix";
+    publisher = "flix";
+    version = "0.64.0";
+    sha256 = "1p76hvhkmmfcgf17qhkvmqwvi07ks62hlhv6sy4js43vrczv5dqw";
+  }
+]
