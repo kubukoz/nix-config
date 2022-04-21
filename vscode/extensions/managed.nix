afd7a9ec8b5bb197904ebb219fc8f1f545090a73
Jakub Kozłowski <kubukoz@gmail.com>
2022-04-21 15:50:16 +0200

    (hmm) Add disneystreaming.smithy: 0.0.4

--- vscode/extensions/managed.nix
+++ vscode/extensions/managed.nix
@@ -3,9 +3,17 @@
 # Don't edit it manually!
 # This file should always parse as an array of attrsets.
 #
-[{
-  name = "metals";
-  publisher = "scalameta";
-  version = "1.13.13";
-  sha256 = "0q4gank1nibzbwl8sdfw0gd985hpr624hjdzh2p2wn3137s2bxmx";
-}]
+[
+  {
+    name = "metals";
+    publisher = "scalameta";
+    version = "1.13.13";
+    sha256 = "0q4gank1nibzbwl8sdfw0gd985hpr624hjdzh2p2wn3137s2bxmx";
+  }
+  {
+    name = "smithy";
+    publisher = "disneystreaming";
+    version = "0.0.4";
+    sha256 = "1nl73rg0nqzgp2r3sv968h4bcv2p4f3lj0pjar3s8jgm4jlln4qi";
+  }
+]
