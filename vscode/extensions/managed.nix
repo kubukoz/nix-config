51c6b3092cf0663183e638682f03ba392fa6051c
Jakub Kozłowski <kubukoz@gmail.com>
2022-07-03 16:35:39 +0200

    (hmm) Update scalameta.metals: 1.17.9 -> 1.17.10, sourcegraph.sourcegraph: 2.2.4 -> 2.2.7

--- vscode/extensions/managed.nix
+++ vscode/extensions/managed.nix
@@ -7,8 +7,8 @@
   {
     name = "metals";
     publisher = "scalameta";
-    version = "1.17.9";
-    sha256 = "16yl0xsnym42rr662g4p6pglk2y2a4z5amwwd83mxnag0n53q07h";
+    version = "1.17.10";
+    sha256 = "0vi51l1g26y027g0ag989ygb4rcn11kmxmgb2b9kwbc328s31n3x";
   }
   {
     name = "smithy";
@@ -19,8 +19,8 @@
   {
     name = "sourcegraph";
     publisher = "sourcegraph";
-    version = "2.2.4";
-    sha256 = "1hfv1cibbw0djqwafgvkv6zxwi80430qdbvid25kbgvjzvc7bal1";
+    version = "2.2.7";
+    sha256 = "1cm15s73rzcv4m0c99wscxky0p6v2h8xckdn0qxhp88nv9k2khq8";
   }
   {
     name = "vscode-direnv";
