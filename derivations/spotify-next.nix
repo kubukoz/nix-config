4fd6728706479a48417c8f9a43ee3f08d4f2a5ab
Jakub Kozłowski <kubukoz@gmail.com>
2022-07-23 01:26:35 +0200

    spotify-next: 1.9.2 → 1.10.0 spotify-next-deps: 1.9.2 → 1.10.0, [31;1m+423.5 KiB[0m

--- derivations/spotify-next.nix
+++ derivations/spotify-next.nix
@@ -1,11 +1,11 @@
 { coursier-tools, installShellFiles, lib }:
 
 let
-  version = "1.9.2";
+  version = "1.10.0";
   pname = "spotify-next";
   completions = builtins.fetchurl {
     url = "https://raw.githubusercontent.com/kubukoz/spotify-next/v${version}/completions.zsh";
-    sha256 = "1ffzfh9m65i5cs7c8v953dn0xampk952f362px146n1p86sgxr4k";
+    sha256 = "sha256:1ffzfh9m65i5cs7c8v953dn0xampk952f362px146n1p86sgxr4k";
   };
 in
 coursier-tools.coursierBootstrap {
@@ -16,5 +16,5 @@ coursier-tools.coursierBootstrap {
     installShellCompletion --name _${pname} --zsh ${completions}
   '';
   mainClass = "com.kubukoz.next.Main";
-  sha256 = "sha256-IRq1WjL61DLMkvzDCG+CIbkJU3rOiu9nSvHEhjT3tGg";
+  sha256 = "sha256-wci/pF7CFPrL7hPW5GxeMFHhbtI6LDPO7ZjeqpGd6lw=";
 }
