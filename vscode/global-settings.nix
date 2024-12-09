3a84df4c0c875b90aaa292438f9d1d6097af3a74
Jakub Kozłowski <kubukoz@gmail.com>
2024-12-09 18:38:10 +0100

    Sort settings

--- vscode/global-settings.nix
+++ vscode/global-settings.nix
@@ -1,29 +1,29 @@
 {
-  "editor.fontFamily" =
-    "'JetBrains Mono', 'Meslo LGS NF', Monaco, 'Courier New', monospace";
-  "terminal.integrated.fontFamily" = "MesloLGS NF";
-  "terminal.integrated.fontSize" = 14;
   "breadcrumbs.enabled" = true;
   "debug.allowBreakpointsEverywhere" = true;
+  "editor.accessibilitySupport" = "off";
   "editor.cursorBlinking" = "solid";
+  "editor.fontFamily" =
+    "'JetBrains Mono', 'Meslo LGS NF', Monaco, 'Courier New', monospace";
   "editor.formatOnSave" = true;
+  "editor.inlineSuggest.enabled" = true;
+  "editor.linkedEditing" = true;
   "editor.minimap.enabled" = false;
   "editor.suggestSelection" = "first";
   "editor.tabSize" = 2;
-  "editor.inlineSuggest.enabled" = true;
-  "editor.linkedEditing" = true;
   "files.autoSave" = "onFocusChange";
   "files.defaultLanguage" = "markdown";
   "files.insertFinalNewline" = true;
   "files.trimTrailingWhitespace" = true;
+  "githubPullRequests.terminalLinksHandler" = "github";
+  "terminal.integrated.fontFamily" = "MesloLGS NF";
+  "terminal.integrated.fontSize" = 14;
+  "typescript.preferences.quoteStyle" = "single";
+  "window.zoomLevel" = 1;
   "workbench.editor.enablePreview" = false;
+  "workbench.editor.highlightModifiedTabs" = true;
   "workbench.editor.limit.enabled" = true;
   "workbench.editor.limit.value" = 10;
   "workbench.editor.showTabs" = "multiple";
-  "workbench.editor.highlightModifiedTabs" = true;
   "workbench.startupEditor" = "none";
-  "window.zoomLevel" = 1;
-  "typescript.preferences.quoteStyle" = "single";
-  "githubPullRequests.terminalLinksHandler" = "github";
-  "editor.accessibilitySupport" = "off";
 }
