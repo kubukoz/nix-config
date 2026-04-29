bf79807106b512b8694fff69c3995e31b45635dd
Jakub Kozłowski <kubukoz@gmail.com>
2026-04-29 22:05:46 +0200

    Remove github mcp for now

--- claude/default.nix
+++ claude/default.nix
@@ -2,25 +2,25 @@
 {
   programs.claude-code = {
     enable = true;
-    mcpServers = {
-      github-com = {
-        type = "stdio";
-        command = "bash";
-        args =
-          let
-            host = "github.com";
-          in
-          [
-            "-c"
-            (builtins.concatStringsSep " " [
-              "GITHUB_PERSONAL_ACCESS_TOKEN=$(gh auth token --hostname=${host})"
-              "GITHUB_HOST=https://${host}"
-              "${pkgs.lib.getExe pkgs.github-mcp-server}"
-              "stdio"
-            ])
-          ];
-      };
-    };
+    # mcpServers = {
+    #   github-com = {
+    #     type = "stdio";
+    #     command = "bash";
+    #     args =
+    #       let
+    #         host = "github.com";
+    #       in
+    #       [
+    #         "-c"
+    #         (builtins.concatStringsSep " " [
+    #           "GITHUB_PERSONAL_ACCESS_TOKEN=$(gh auth token --hostname=${host})"
+    #           "GITHUB_HOST=https://${host}"
+    #           "${pkgs.lib.getExe pkgs.github-mcp-server}"
+    #           "stdio"
+    #         ])
+    #       ];
+    #   };
+    # };
   };
 
   home.packages =
